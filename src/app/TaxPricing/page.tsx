5891bdd6139d425022782209651edb2c522c73d4
joeb97666 <joeb97666@gmail.com>
2024-01-16 22:04:42 -0800

    update4

--- src/app/TaxPricing/page.tsx
+++ src/app/TaxPricing/page.tsx
@@ -1,8 +1,7 @@
 'use client'
-import Image from 'next/image'
 import { V0Navigatior } from '@/components/v0-navigator'
 import { TaxPricing } from '@/components/TaxPricing'
-import { PriceList } from '@/components/PriceList'
+
 
 export default function WebDev() {
   return (
